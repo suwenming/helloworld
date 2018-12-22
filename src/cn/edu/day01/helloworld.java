083e0c06dd7fcff4a36219048870242410ebf95e
suwenming <max.su@foxmail.com>
2018-12-23 05:49:51 +0800

    this is my first gitproject!

---
+++ src/cn/edu/day01/helloworld.java
@@ -0,0 +1,14 @@
+package cn.edu.day01;
+
+import java.util.Scanner;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		sc.close();
+		System.out.println(name);
+	}
+}
